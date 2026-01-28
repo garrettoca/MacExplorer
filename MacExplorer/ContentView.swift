8abe543ceb898da46fd33582096fca3c033a6ee2
Garrett (Mac) <garrett.ocarroll@gmail.com>
2026-01-28 16:40:57 +0000

    Initial Commit

---
+++ MacExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacExplorer
+//
+//  Created by Garrett O'Carroll on 28/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacExplorer/MacExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacExplorerApp.swift
+//  MacExplorer
+//
+//  Created by Garrett O'Carroll on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
